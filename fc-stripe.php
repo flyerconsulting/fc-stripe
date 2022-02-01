04cdaa409272912d7f29dfa75fb1b67ce5eb4b8c
Christopher Brown <cdbrown2018@gmail.com>
2022-01-31 20:55:50 -0500

    Change ownership

--- fc-stripe.php
+++ fc-stripe.php
@@ -7,8 +7,8 @@
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Christopher Brown
- * Author URI:        https://author.example.com/
+ * Author:            Flyer Consulting
+ * Author URI:        https://www.flyerconsulting.om/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
